c7db43fdafab4914b41e1c46305b6adf655c4ed8
Abhinash Dey <night20games@gmail.com>
2026-03-27 14:58:49 +0530

    fixed url

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -116,7 +116,7 @@ export default function authRouter(redisClient, googleClient, astraDB) {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
-        redirect_uri: "http://localhost:5000/api/auth/github/callback",
+        redirect_uri: "https://iniyaai-backend.onrender.com/api/auth/github/callback",
       },
       {
         headers: {
